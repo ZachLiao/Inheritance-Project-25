e3e9b3f7226108f504e7623eca6a516b0b5572fc
geffenk <gk3048@pleasantonusd.net>
2025-03-18 12:07:51 -0700

    add Game.java variables and constructet

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,39 @@
 public class Game {
-    
+    private double payout;
+    private int timesPlayed;
+    private String name;
+    private int betSize;
+    public Game(String name, int betSize, double payout){
+        this.name = name;
+        this.betSize = betSize;
+        this.payout = payout;
+        this.timesPlayed = 0;
+    }
+    public int getBetSize(){
+        return betSize;
+    }
+    public void setBetSize(int bet){
+        betSize = bet;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public double getPayout(){
+        return payout;
+    }
+    public void setPayout(double d){
+        payout = d;
+    }
+    public int getTimesPlayed(){
+        return timesPlayed;
+    }
+    public void setTimesPlayed(int t){
+        timesPlayed = t;
+    }
+    public String toString(){
+        return name + " is a game with a payout of " + payout + " and a current bet size of " + betSize + " and has been played " + timesPlayed + " times";
+    }
 }
