1f4b14df62adf93aac84b987b5897d0a60117afb
geffenk <gk3048@pleasantonusd.net>
2025-03-18 12:38:56 -0700

    slots.java changes

--- src/Slots.java
+++ src/Slots.java
@@ -1,9 +1,22 @@
 public class Slots extends Game{
     private int columns;
     private int numberOfSymbols;
+    private String[] symbolsToChooseFrom = {"7", "bell", "bar", "bell", "cherry", "clover", "lemon", "diamond", "orange", "watermelon", "horseshoe", "lime"};
     private String[] symbols;
-    public Slots(String name, int betSize, double payout){
+    public Slots(String name, int betSize, double payout, int columns, int numberOfSymbols){
         super(name, betSize, payout);
         setTimesPlayed(0);
+        this.numberOfSymbols = numberOfSymbols;
+        this.columns = columns;
+        if (numberOfSymbols<=12 && numberOfSymbols>0){
+            symbols = new String[numberOfSymbols];
+            for (int i=0; i<numberOfSymbols; i++){
+                symbols[i] = symbolsToChooseFrom[i];
+            }
+        }
+        else{
+            System.out.println("An out of bounds number of symbols was imputed so the number of symbols has been defaulted to 12");
+            symbols = symbolsToChooseFrom;
+        }
     }
 }
