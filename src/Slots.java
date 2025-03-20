6346e64b0e83cb008139b97f8073000a3e2b0588
Zach Liao <yuyangliao2009@gmail.com>
2025-03-20 13:48:39 -0700

    Updated Slots.java

--- src/Slots.java
+++ src/Slots.java
@@ -1,23 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Slots extends Game{
+    private Scanner in = new Scanner(System.in);
+    private int choice;
     private int columns;
     private int numberOfSymbols;
     private String[] symbolsToChooseFrom = {"7", "bell", "bar", "bell", "cherry", "clover", "lemon", "diamond", "orange", "watermelon", "horseshoe", "lime"};
     private String[] symbols;
-    public Slots(String name, int betSize, double payout, int columns, int numberOfSymbols){
+    public Slots(String name, int betSize, double payout){
         super(name, betSize, payout);
         setTimesPlayed(0);
-        this.numberOfSymbols = numberOfSymbols;
-        this.columns = columns;
-        if (numberOfSymbols<=12 && numberOfSymbols>0){
-            symbols = new String[numberOfSymbols];
-            for (int i=0; i<numberOfSymbols; i++){
-                symbols[i] = symbolsToChooseFrom[i];
-            }
+        intro();
+        symbols = new String[numberOfSymbols];
+        for (int i=0; i<numberOfSymbols; i++){
+            symbols[i] = symbolsToChooseFrom[i];
         }
-        else{
-            System.out.println("An out of bounds number of symbols was imputed so the number of symbols has been defaulted to 12");
-            symbols = symbolsToChooseFrom;
-        }
+        
     }
     public int getColumns(){
         return columns;
@@ -38,5 +38,49 @@ public class Slots extends Game{
     @Override
     public void play(){
 
+    }
+    public void intro() {
+        System.out.println("""
+                You chose to play Slots. You may choose from 3 to 7 columns.
+                Type a number 3 through 7 to confirm your choice.
+                """);
+        while (true) {
+            String input = in.nextLine();
+            Pattern pattern = Pattern.compile("\\d+");
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()) {
+                int number = Integer.parseInt(matcher.group());
+                if (number > 2 && number < 8) {
+                    columns = number;
+                    break;
+                }
+                System.out.println("Not a valid choice");
+            } else {
+                System.out.println("No number found.");
+                System.out.println("Please try again");
+            }
+        }
+        System.out.println("""
+                Now please choose from 3 to 12 symbols in each column.
+                Type a number from 3 to 12 to confirm your choice.
+                """);
+        while (true) {
+            String input = in.nextLine();
+            Pattern pattern = Pattern.compile("\\d+");
+            Matcher matcher = pattern.matcher(input);
+        
+            if (matcher.find()) {
+                int number = Integer.parseInt(matcher.group());
+                if (number > 2 && number < 13) {
+                    numberOfSymbols = number;
+                    break;
+                }
+                System.out.println("Not a valid choice");
+            } else {
+                System.out.println("No number found.");
+                System.out.println("Please try again");
+            }
+        }
     }
 }
