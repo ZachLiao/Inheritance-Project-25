2ac17c16da43f3722ee430edb9a1700acd330a1f
Zach Liao <yuyangliao2009@gmail.com>
2025-03-20 13:25:45 -0700

    Formatted Game.java

--- src/Game.java
+++ src/Game.java
@@ -3,40 +3,52 @@ public class Game {
     private int timesPlayed;
     private String name;
     private int betSize;
-    public Game(String name, int betSize, double payout){
+
+    public Game(String name, int betSize, double payout) {
         this.name = name;
         this.betSize = betSize;
         this.payout = payout;
         this.timesPlayed = 0;
     }
-    public int getBetSize(){
+
+    public int getBetSize() {
         return betSize;
     }
-    public void setBetSize(int bet){
+
+    public void setBetSize(int bet) {
         betSize = bet;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String n){
+
+    public void setName(String n) {
         name = n;
     }
-    public double getPayout(){
+
+    public double getPayout() {
         return payout;
     }
-    public void setPayout(double d){
+
+    public void setPayout(double d) {
         payout = d;
     }
-    public int getTimesPlayed(){
+
+    public int getTimesPlayed() {
         return timesPlayed;
     }
-    public void setTimesPlayed(int t){
+
+    public void setTimesPlayed(int t) {
         timesPlayed = t;
     }
-    public String toString(){
-        return name + " is a game with a payout of " + payout + " and a current bet size of " + betSize + " and has been played " + timesPlayed + " times";
+
+    public String toString() {
+        return name + " is a game with a payout of " + payout + " and a current bet size of " + betSize
+                + " and has been played " + timesPlayed + " times";
     }
-    public void play(){
+
+    public void play() {
         timesPlayed++;
     }
 }
