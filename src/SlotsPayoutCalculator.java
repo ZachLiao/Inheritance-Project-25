db7bd63773980123723730556fac7883a1bf4fa0
geffenk <gk3048@pleasantonusd.net>
2025-03-22 10:23:56 -0700

    started slotspayoutcalculator

--- src/SlotsPayoutCalculator.java
+++ src/SlotsPayoutCalculator.java
@@ -1,6 +1,16 @@
 public class SlotsPayoutCalculator {
     
-    public static int calculatePayout(){
+    public static int calculatePayout(String[] a, int col, int sym, int bet){
+        boolean allMatch = true;
+        for (int i=0; i<a.length-1; i++){
+            if (!a[i].equals(a[i+1])){
+                allMatch = false;
+            }
+        }
+        if(allMatch){
+            //probability of all match is (1/sym)^(col-1)
+            return (int)(bet*1/((1/sym)^(col-1))*.9);
+        }
         return 3;
     }
 }
